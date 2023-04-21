064733d2541dd74708500572ec8135b27e69a5cd
rakeshsharma95 <rksharmajpr95@gmail.com>
2023-04-21 16:50:12 +0530

    add grpc data and add grpc gateway

---
+++ gapi/converter.go
@@ -0,0 +1,17 @@
+package gapi
+
+import (
+	db "github.com/rdevelop/simplebank/db/sqlc"
+	"github.com/rdevelop/simplebank/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.Fullname,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
